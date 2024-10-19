e6db100a72a4af428efedf2a8d7a237523d18fd6
y3u <79641144+y3u@users.noreply.github.com>
2024-10-19 17:27:32 +0900

    Update ScreenEditor.js

--- ScreenEditor.js
+++ ScreenEditor.js
@@ -1,37 +1,28 @@
-import React, { useState } from 'react';
+export class ScreenEditor {
+    render(container) {
+        container.innerHTML = `
+            <h2>Screen Editor</h2>
+            <input type="file" id="imageUpload" accept="image/*">
+            <div id="imagePreview"></div>
+        `;
 
-export function ScreenEditor() {
-  const [images, setImages] = useState([]);
-  const [selectedImage, setSelectedImage] = useState(null);
+        this.addEventListeners(container);
+    }
 
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      setImages([...images, { src: e.target.result, name: file.name }]);
-    };
-    reader.readAsDataURL(file);
-  };
-
-  return (
-    <div className="screen-editor">
-      <input type="file" accept="image/png" onChange={handleImageUpload} />
-      <div className="image-list">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image.src}
-            alt={image.name}
-            onClick={() => setSelectedImage(image)}
-          />
-        ))}
-      </div>
-      {selectedImage && (
-        <div className="image-editor">
-          <img src={selectedImage.src} alt={selectedImage.name} />
-          {/* ここに画像編集ツールを追加 */}
-        </div>
-      )}
-    </div>
-  );
+    addEventListeners(container) {
+        container.querySelector('#imageUpload').addEventListener('change', (event) => {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    const img = document.createElement('img');
+                    img.src = e.target.result;
+                    img.style.maxWidth = '100%';
+                    container.querySelector('#imagePreview').innerHTML = '';
+                    container.querySelector('#imagePreview').appendChild(img);
+                };
+                reader.readAsDataURL(file);
+            }
+        });
+    }
 }
