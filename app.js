c21187f283a667f08f75b6da571087236c44a90e
y3u <79641144+y3u@users.noreply.github.com>
2024-10-19 17:27:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-import React, { useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { AREditor } from './components/AREditor';
-import { ScreenEditor } from './components/ScreenEditor';
-import { ModelList } from './components/ModelList';
-import { QRCodeGenerator } from './components/QRCodeGenerator';
+import { AREditor } from './AREditor.js';
+import { ScreenEditor } from './ScreenEditor.js';
+import { ModelList } from './ModelList.js';
+import { QRCodeGenerator } from './QRCodeGenerator.js';
 
-export default function App() {
-  const [selectedTab, setSelectedTab] = useState('ar');
+class App {
+    constructor() {
+        this.arEditor = new AREditor();
+        this.screenEditor = new ScreenEditor();
+        this.modelList = new ModelList();
+        this.qrCodeGenerator = new QRCodeGenerator();
 
-  return (
-    <div className="app">
-      <div className="tabs">
-        <button onClick={() => setSelectedTab('ar')}>AR編集</button>
-        <button onClick={() => setSelectedTab('screen')}>画面編集</button>
-      </div>
-      <div className="main-content">
-        <div className="left-panel">
-          {selectedTab === 'ar' ? (
-            <Canvas>
-              <AREditor />
-            </Canvas>
-          ) : (
-            <ScreenEditor />
-          )}
-        </div>
-        <div className="right-panel">
-          <ModelList />
-          <QRCodeGenerator />
-        </div>
-      </div>
-    </div>
-  );
+        this.render();
+    }
+
+    render() {
+        const appDiv = document.getElementById('app');
+        appDiv.innerHTML = `
+            <div class="editor">
+                <h1>WebAR Editor</h1>
+                <div id="arEditor"></div>
+                <div id="screenEditor"></div>
+                <div id="modelList"></div>
+                <div id="qrCodeGenerator"></div>
+            </div>
+            <div class="preview" id="preview"></div>
+        `;
+
+        this.arEditor.render(document.getElementById('arEditor'));
+        this.screenEditor.render(document.getElementById('screenEditor'));
+        this.modelList.render(document.getElementById('modelList'));
+        this.qrCodeGenerator.render(document.getElementById('qrCodeGenerator'));
+    }
 }
+
+new App();
