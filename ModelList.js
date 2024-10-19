1412155760b34153c73dbeb8d5cabd5c9579534c
y3u <79641144+y3u@users.noreply.github.com>
2024-10-19 17:46:54 +0900

    Update ModelList.js

--- ModelList.js
+++ ModelList.js
@@ -9,6 +9,19 @@ export class ModelList {
             <ul>
                 ${this.models.map(model => `<li>${model}</li>`).join('')}
             </ul>
+            <input type="file" id="modelUpload" accept=".glb,.gltf">
         `;
+
+        this.addEventListeners(container);
+    }
+
+    addEventListeners(container) {
+        container.querySelector('#modelUpload').addEventListener('change', (event) => {
+            const file = event.target.files[0];
+            if (file) {
+                this.models.push(file.name);
+                this.render(container);
+            }
+        });
     }
 }
